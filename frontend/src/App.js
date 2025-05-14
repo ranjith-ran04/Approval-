6c81131deeed11a6713791d7b252e4b6d8972c16
thamarainagasamy <thamarainagasamy2006@gmail.com>
2025-05-14 23:57:11 +0530

    nav bar created

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Home from './components/Home.js'
+
+function App() {
+  return (
+    <div>
+    <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import './home.css'
+import NavigationBar from '../widgets/NavigationBar';
+
+function Home() {
+    return(
+        <div id = "container">
+        <NavigationBar text = {'DIRECTORATE OF TECHNICAL EDUCATION \nTAMILNADU LATERAL ENTRY B.E/B.TECH ADMISSIONS-2025 \nAPPROVAL PROCESS'} profile = {true}/>
+        </div>
+    )
+}
+
+export default Home;
