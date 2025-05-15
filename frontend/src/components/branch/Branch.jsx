a2d9fbfec019a2963a98a5499efd8fa9a0021169
Hasan Hasim <hasim30042005@gmail.com>
2025-05-15 22:54:28 +0530

    draft2

--- frontend/src/components/branch/Branch.jsx
+++ frontend/src/components/branch/Branch.jsx
@@ -1,75 +1,161 @@
-import './branch.css'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./branch.css";
 
-function Branch(){
-    return(
-        <div id='box'>
-            <h2 className='heading'>BRANCH DETAILS</h2>
-            <div className="table">
-                <div className="table-header">
-                <div>Edit</div>
-                <div>Branch Code</div>
-                <div>Branch Name</div>
-                <div>Approved In Take</div>
-                <div>First Year Admitted</div>
-                <div>Discontinued</div>
-                <div>Transferred Students From Your College</div>
-                <div>Transferred Students To Your College</div>
-                <div>LAP</div>
-                <div>Year of Starting the Course</div>
-                <div>Accredition Valid Upto</div>
-                <div>NBA Status</div>
-                <div>Delete</div>
-                </div>
-
-                {branchData.map((branch, index) => (
-                <div className="table-row" key={index}>
-                    <div><button className="edit-btn">Edit</button></div>
-                    <div>{branch.code}</div>
-                    <div>{branch.name}</div>
-                    <div>{branch.approvedIntake}</div>
-                    <div>{branch.firstYearAdmitted}</div>
-                    <div>{branch.discontinued}</div>
-                    <div>{branch.transferredFrom}</div>
-                    <div>{branch.transferredTo}</div>
-                    <div>{branch.lap}</div>
-                    <div>{branch.yearStart}</div>
-                    <div>{branch.accUpto}</div>
-                    <div>{branch.nba}</div>
-                    <div><button className="delete-btn">Delete</button></div>
-                </div>
-                ))}
-            </div>
+function Branch() {
+  return (
+    <div id="box">
+      <div className="first">
+        <h2 className="heading">BRANCH DETAILS</h2>
+        <button className="addBranch-btn">Add Branch</button>
+      </div>
+      <div className="table">
+        <div className="table-header">
+          <div>Edit</div>
+          <div>Branch Code</div>
+          <div>Branch Name</div>
+          <div>Approved In Take</div>
+          <div>First Year Admitted</div>
+          <div>Discontinued</div>
+          <div>Transferred Students From Your College</div>
+          <div>Transferred Students To Your College</div>
+          <div>LAP</div>
+          <div>Year of Starting the Course</div>
+          <div>Accredition Valid Upto</div>
+          <div>NBA Status</div>
+          <div>Delete</div>
         </div>
-    )
+
+        {branchData.map((branch, index) => (
+          <div className="table-row" key={index}>
+            <div>
+              <button className="edit-btn">Edit</button>
+            </div>
+            <div>{branch.code}</div>
+            <div>{branch.name}</div>
+            <div>{branch.approvedIntake}</div>
+            <div>{branch.firstYearAdmitted}</div>
+            <div>{branch.discontinued}</div>
+            <div>{branch.transferredFrom}</div>
+            <div>{branch.transferredTo}</div>
+            <div>{branch.lap}</div>
+            <div>{branch.yearStart}</div>
+            <div>{branch.accUpto}</div>
+            <div>{branch.nba}</div>
+            <div>
+              <button className="delete-btn">Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 const branchData = [
-    {
-      code: "CM",
-      name: "COMPUTER SCIENCE AND ENGINEERING (SS)",
-      approvedIntake: 118,
-      firstYearAdmitted: 117,
-      discontinued: 2,
-      transferredFrom: 0,
-      transferredTo: 0,
-      lap: 1,
-      yearStart: 2006,
-      accUpto: 2029,
-      nba: "Yes"
-    },
-    {
-      code: "CS",
-      name: "COMPUTER SCIENCE AND ENGINEERING",
-      approvedIntake: 59,
-      firstYearAdmitted: 58,
-      discontinued: 0,
-      transferredFrom: 0,
-      transferredTo: 0,
-      lap: 1,
-      yearStart: 1982,
-      accUpto: 2029,
-      nba: "Yes"
-    }
-    // Add more rows if needed
-  ];
+  {
+    code: "CM",
+    name: "COMPUTER SCIENCE AND ENGINEERING (SS)",
+    approvedIntake: 118,
+    firstYearAdmitted: 117,
+    discontinued: 2,
+    transferredFrom: 0,
+    transferredTo: 0,
+    lap: 1,
+    yearStart: 2006,
+    accUpto: 2029,
+    nba: "Yes",
+  },
+  {
+    code: "CS",
+    name: "COMPUTER SCIENCE AND ENGINEERING",
+    approvedIntake: 59,
+    firstYearAdmitted: 58,
+    discontinued: 0,
+    transferredFrom: 0,
+    transferredTo: 0,
+    lap: 1,
+    yearStart: 1982,
+    accUpto: 2029,
+    nba: "Yes",
+  },
+  {
+    code: "CM",
+    name: "COMPUTER SCIENCE AND ENGINEERING (SS)",
+    approvedIntake: 118,
+    firstYearAdmitted: 117,
+    discontinued: 2,
+    transferredFrom: 0,
+    transferredTo: 0,
+    lap: 1,
+    yearStart: 2006,
+    accUpto: 2029,
+    nba: "Yes",
+  },
+  {
+    code: "CS",
+    name: "COMPUTER SCIENCE AND ENGINEERING",
+    approvedIntake: 59,
+    firstYearAdmitted: 58,
+    discontinued: 0,
+    transferredFrom: 0,
+    transferredTo: 0,
+    lap: 1,
+    yearStart: 1982,
+    accUpto: 2029,
+    nba: "Yes",
+  },
+  {
+    code: "CM",
+    name: "COMPUTER SCIENCE AND ENGINEERING (SS)",
+    approvedIntake: 118,
+    firstYearAdmitted: 117,
+    discontinued: 2,
+    transferredFrom: 0,
+    transferredTo: 0,
+    lap: 1,
+    yearStart: 2006,
+    accUpto: 2029,
+    nba: "Yes",
+  },
+  {
+    code: "CS",
+    name: "COMPUTER SCIENCE AND ENGINEERING",
+    approvedIntake: 59,
+    firstYearAdmitted: 58,
+    discontinued: 0,
+    transferredFrom: 0,
+    transferredTo: 0,
+    lap: 1,
+    yearStart: 1982,
+    accUpto: 2029,
+    nba: "Yes",
+  },
+  {
+    code: "CM",
+    name: "COMPUTER SCIENCE AND ENGINEERING (SS)",
+    approvedIntake: 118,
+    firstYearAdmitted: 117,
+    discontinued: 2,
+    transferredFrom: 0,
+    transferredTo: 0,
+    lap: 1,
+    yearStart: 2006,
+    accUpto: 2029,
+    nba: "Yes",
+  },
+  {
+    code: "CS",
+    name: "COMPUTER SCIENCE AND ENGINEERING",
+    approvedIntake: 59,
+    firstYearAdmitted: 58,
+    discontinued: 0,
+    transferredFrom: 0,
+    transferredTo: 0,
+    lap: 1,
+    yearStart: 1982,
+    accUpto: 2029,
+    nba: "Yes",
+  },
+  // Add more rows if needed
+];
 
-export default Branch
+export default Branch;
