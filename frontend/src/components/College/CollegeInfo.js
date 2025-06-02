f8e92eeff4076d392f989666c50a2e4504f111b4
ranjith-ran04 <d.ranjith042005@gmail.com>
2025-06-02 22:58:18 +0530

    widget

--- frontend/src/components/College/CollegeInfo.js
+++ frontend/src/components/College/CollegeInfo.js
@@ -11,6 +11,7 @@ export const CollegeInfo = () => {
   const [showAlert, setShowAlert] = useState(false);
   const [bool , setBool] = useState(false)
 
+
   const handleCloseAlert = () => {
     setShowAlert(false);
   };
