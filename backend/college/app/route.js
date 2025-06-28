61c3fe0102bad270253afd2f2ac7a661bb76424c
thamarainagasamy <thamarainagasamy2006@gmail.com>
2025-06-28 23:37:19 +0530

    route

--- backend/college/app/route.js
+++ backend/college/app/route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {formb} = require("./forms/form_b");
+const formb = require("./forms/form_b");
 
 router.post("/formb", formb);
 
