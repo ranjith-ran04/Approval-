3d6fdb997b058d1b44d5241f2fc02a44acafe181
ranjith-ran04 <d.ranjith042005@gmail.com>
2025-06-29 15:06:52 +0530

    form started

--- backend/college/app/config/db.js
+++ backend/college/app/config/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Kanna@2006",
-  database: "approvalsample",
+  password: "Ranjith@123",
+  database: "approval2025",
 });
 
 connection.connect((err) => {
