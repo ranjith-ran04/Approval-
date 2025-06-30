229a818d51abd5a6b59bd2ea7ce4c665b1b68470
thamarainagasamy <thamarainagasamy2006@gmail.com>
2025-06-30 15:58:09 +0530

    changes

--- backend/college/app/route.js
+++ backend/college/app/route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const formb = require("./forms/form_b");
+const home = require('../app/controllers/home')
 
 // router.get("/forma", forma);
 router.post("/formb", formb);
+router.post("/home",home);
 
 module.exports = router;
