234451e4665c34291f0a37b0a55cb0c83ef56a02
ranjith-ran04 <d.ranjith042005@gmail.com>
2025-06-30 22:40:43 +0530

    form_tnlea finished

--- backend/college/app/route.js
+++ backend/college/app/route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const formb = require("./forms/form_b");
+const form_tnlea = require("./forms/form_tnlea");
 
 // router.get("/forma", forma);
 router.post("/formb", formb);
+router.post("/formlea",form_tnlea)
 
 module.exports = router;
