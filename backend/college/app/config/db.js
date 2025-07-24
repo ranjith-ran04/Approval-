3482672c97e455e23cd9b183437fd5f9f11b20cb
Hasan Hasim <hasim30042005@gmail.com>
2025-07-24 21:46:24 +0530

    form logo changes in thamarai branch

--- backend/college/app/config/db.js
+++ backend/college/app/config/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Kanna@2006",
-  database: "approvalsample", 
+  password: "oracle@hasan30",
+  database: "aprv_old", 
 });
 
 connection.connect((err) => {
--- frontend/src/widgets/sidebar/Sidebar.jsx
+++ frontend/src/widgets/sidebar/Sidebar.jsx
@@ -6,26 +6,69 @@ import handleForm from "../sidebar/pdfApi";
 function Sidebar({ setCurrent }) {
   const items = [
     { id: 0, iconId: "homeIcon", label: "Home", action: () => setCurrent(0) },
-    { id: 1, iconId: "collegeIcon", label: "College Details", action: () => setCurrent(1) },
-    { id: 2, iconId: "branchIcon", label: "Branchwise Details", action: () => setCurrent(2) },
-    { id: 3, iconId: "studentIcon", label: "Student Details", action: () => setCurrent(5)},
+    {
+      id: 1,
+      iconId: "collegeIcon",
+      label: "College Details",
+      action: () => setCurrent(1),
+    },
+    {
+      id: 2,
+      iconId: "branchIcon",
+      label: "Branchwise Details",
+      action: () => setCurrent(2),
+    },
+    {
+      id: 3,
+      iconId: "studentIcon",
+      label: "Student Details",
+      action: () => setCurrent(5),
+    },
     { id: 4, iconId: "discontinuedIcon", label: "Discontinued Details" },
-    { id: 5, iconId: "formIcon", label: "Form A", action: () => handleForm("forma") },
-    { id: 6, iconId: "formIcon", label: "Form B", action: () => handleForm("formb") },
-    { id: 7, iconId: "formIcon", label: "Form C", action: () => handleForm("formc") },
-    { id: 8, iconId: "formIcon", label: "Form D" },
-    { id: 9, iconId: "formIcon", label: "Form FG" },
-    { id: 10, iconId: "formIcon", label: "Form LEA2025", action:()=> handleForm("formlea")},
+    {
+      id: 5,
+      iconId: "formIcon1",
+      label: "Form A",
+      action: () => handleForm("forma"),
+    },
+    {
+      id: 6,
+      iconId: "formIcon2",
+      label: "Form B",
+      action: () => handleForm("formb"),
+    },
+    {
+      id: 7,
+      iconId: "formIcon3",
+      label: "Form C",
+      action: () => handleForm("formc"),
+    },
+    { id: 8, iconId: "formIcon4", label: "Form D" },
+    { id: 9, iconId: "formIcon5", label: "Form FG" },
+    {
+      id: 10,
+      iconId: "formIcon6",
+      label: "Form LEA2025",
+      action: () => handleForm("formlea"),
+    },
   ];
 
   const [collapsed, setCollapsed] = useState(false);
-  const [activeItems, setActiveItems] = useState([true,false,false,false,false]);
+  const [activeItems, setActiveItems] = useState([
+    true,
+    false,
+    false,
+    false,
+    false,
+  ]);
 
-  const toggleSidebar = () => setCollapsed(prev => !prev);
+  const toggleSidebar = () => setCollapsed((prev) => !prev);
 
   const handleItemClick = (index, action) => {
-    if(index<5)
-    setActiveItems(prev => prev.map((_, i) => i === index ? true : false));
+    if (index < 5)
+      setActiveItems((prev) =>
+        prev.map((_, i) => (i === index ? true : false))
+      );
     if (action) action();
   };
 
@@ -34,18 +77,18 @@ function Sidebar({ setCurrent }) {
       <div id="iconDiv" onClick={toggleSidebar}>
         <div id="listIcon"></div>
       </div>
-
-      {items.map((item, index) => (
-        <div
-          key={item.id}
-          className={`menuItems ${activeItems[index] ? "active" : ""}`}
-          onClick={() => handleItemClick(index, item.action)}
-        >
-          <div id={item.iconId}></div>
-          {!collapsed && item.label}
-        </div>
-      ))}
-
+      <div className="itemsDiv">
+        {items.map((item, index) => (
+          <div
+            key={item.id}
+            className={`menuItems ${activeItems[index] ? "active" : ""}`}
+            onClick={() => handleItemClick(index, item.action)}
+          >
+            <div id={item.iconId}></div>
+            {!collapsed && item.label}
+          </div>
+        ))}
+      </div>
       {!collapsed && (
         <div id="buttonDiv">
           <Button name="SUBMIT" />
