8d7809dfd2f922e4df276f871f8140c6ab9fd3e1
ranjith-ran04 <d.ranjith042005@gmail.com>
2025-08-03 13:35:44 +0530

    before push

--- backend/college/app/controllers/login.js
+++ backend/college/app/controllers/login.js
@@ -21,13 +21,13 @@ const login = (req, res) => {
     const token = jwt.sign(
       { counsellingCode: user.c_code },
       process.env.JWT_SECRET,
-      { expiresIn: "10m" }
+      { expiresIn: "60m" }
     );
 
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      maxAge: 10 * 60 * 1000,
+      maxAge: 60 * 60 * 1000,
     });
 
     return res.status(200).json({ changed: user.changed });
