5bd77fff2ed6420ed1ec5290091040c55f47b9ad
Hasan Hasim <hasim30042005@gmail.com>
2025-08-12 17:57:56 +0530

    admin frontend

--- frontend/src/admin/dashboard/Dashboard.jsx
+++ frontend/src/admin/dashboard/Dashboard.jsx
@@ -7,10 +7,12 @@ import { adminhost } from "../../constants/backendpath.js";
 import { useNavigate, useLocation } from "react-router-dom";
 import StudentDetails from "../../components/studentDetails/StudentDetails.jsx";
 import ScrollToTop from "../../widgets/scrollToTop/ScrollToTop.jsx";
+import Chart from '../../widgets/chart/PieChartsDashboard.js';
 
 function Dashboard() {
   const [current, setCurrent] = useState(0);
   const [state, setState] = useState({});
+  const [supp,setSupp] = useState(false);
   const scrollRef = useRef();  
   const location = useLocation();
   console.log(location.state);
@@ -33,6 +35,16 @@ function Dashboard() {
     fetch();
   }, []);
 
+  useEffect(()=>{
+    if(current === 2){
+      setSupp(true);
+    }else{
+      setSupp(false);
+    }
+  },[current]);
+
+console.log(supp);
+
   return logged ? (
     <div className="dashboard">
       <Sidebar setCurrent={setCurrent} admin={true} />
@@ -45,11 +57,15 @@ Tamilnadu Lateral Entry Direct Second Year B.E/B.Tech.,Approval-2025`}
           bool={false}
           setCurrent={setCurrent}
           admin={true}
+          style={{height:'40px'}}
         />
 
         <div className="dashboard-body" ref={scrollRef}>
         <>
-        {current === 1 && <StudentDetails admin={true}/>}
+        {current === 0 && <Chart/>}
+        {current === 1 && <StudentDetails admin={true} supp={supp}/>}
+        {current === 2 && <StudentDetails admin={true} supp={supp}/>}
+
         <ScrollToTop scrollRef={scrollRef} />
         </>
         </div>
