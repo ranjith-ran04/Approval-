a08867dc10cd0096ec2725bd7b3292fb8b462341
Hasan Hasim <hasim30042005@gmail.com>
2025-08-25 15:39:02 +0530

    main pull origin

--- backend/college/app/config/db.js
+++ backend/college/app/config/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2/promise");
 const connection = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Ranjith@123",
-  database: "approval2025",
+  password: "oracle@hasan30",
+  database: "aprv_old",
 });
 
 
