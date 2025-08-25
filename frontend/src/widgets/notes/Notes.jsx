2f18a361b52ee8380a8e4c608ca1948de00946c5
Hasan Hasim <hasim30042005@gmail.com>
2025-08-25 17:13:44 +0530

    notes

--- frontend/src/widgets/notes/Notes.jsx
+++ frontend/src/widgets/notes/Notes.jsx
@@ -30,8 +30,34 @@ function Notes() {
             ×
           </button>
           <div id="para">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua...
+            <p>• Check the College details and modify the required fields.</p>
+            <p>
+              • All allotted students details will get displayed after adding
+              respective branches only.
+            </p>
+            <p>• Add Branch details first and then add student details.</p>
+            <p>
+              • If any of the students has not joined, remove that name using
+              delete option.
+            </p>
+
+            <p>
+              • In Student details,{" "}
+              <span style={{ color: "#32CD32" }}>Green colour field</span> denotes
+              the students with complete details and{" "}
+              <span style={{ color: "yellow" }}>yellow colour field</span>{" "}
+              denotes the students with incomplete details.
+            </p>
+
+            <p>
+              • For Management students' Register number (Create your own
+              11-digit number), Fill the Application number in valid format:
+              <br />
+              (First 4 digits → your college code, next 2 digits → your branch
+              code, next 2 digits → 24 (Year), last 3 digits → unique number).
+              <br />
+              <b>Eg:</b> 5901CS24001
+            </p>
           </div>
         </div>
       )}
