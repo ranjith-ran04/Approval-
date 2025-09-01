e06eed3201f362f363cb84e69d322b947055b01d
Vishnu040912 <vishnu20060312@gmail.com>
2025-09-01 18:08:05 +0530

    c

--- backend/admin/app/config/db.js
+++ backend/admin/app/config/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2/promise");
 const connection = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "Kanna@2006",
-  database: "approval2025", 
+  password: "Vishnu12*#",
+  database: "approval_new", 
 });
 
 
